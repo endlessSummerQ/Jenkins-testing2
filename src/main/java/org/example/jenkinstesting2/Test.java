8e478f4cc7b866a2f94c816507695d12dc553b8f
g.fedonov <g.fedonov@astondevs.ru>
2024-10-07 17:16:59 +0400

    ddd

---
+++ src/main/java/org/example/jenkinstesting2/Test.java
@@ -0,0 +1,38 @@
+package org.example.jenkinstesting2;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Component
+public class Test {
+
+    static int a = 12,c = 12;
+    static int b = 12;
+
+    public Test test() {
+        return this;
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
+//        String invalidUrl = "<host>:8080/some/path"; // Неправильный формат
+//        URI uri = new URI(invalidUrl); // Это вызовет IllegalArgumentException
+        Test test = new Test();
+        System.out.println(test.test());
+    }
+
+    public static void aVoid() {
+        b();
+    }
+
+    private static void b() {
+        throw new RuntimeException("Opps");
+    }
+
+    private static Object c() {
+        return new BigInteger("3");
+    }
+}
